fa10647f89ab3abc0c7ded53418a5d065a89e8f6
Arpitha Bhat <arpithabht12@gmail.com>
2021-09-20 21:43:50 +0530

    MODIFIED: added getters and setters

--- OpenCSVandGson/src/main/java/com/bridgelabz/csvandgson/CSVUser.java
+++ OpenCSVandGson/src/main/java/com/bridgelabz/csvandgson/CSVUser.java
@@ -15,6 +15,38 @@ public class CSVUser {
     @CsvBindByName
     private String country;
 
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPhoneNo() {
+		return phoneNo;
+	}
+
+	public void setPhoneNo(String phoneNo) {
+		this.phoneNo = phoneNo;
+	}
+
+	public String getCountry() {
+		return country;
+	}
+
+	public void setCountry(String country) {
+		this.country = country;
+	}
+
 	@Override
 	public String toString() {
 		return "CSVUser [name=" + name + ", email=" + email + ", phoneNo=" + phoneNo + ", country=" + country + "]";
